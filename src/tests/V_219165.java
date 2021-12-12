209a59ad44d982bc662d6e950798518a6c6cf7b1
Olya Chernukhina <42997933+trnsprntt@users.noreply.github.com>
2021-12-13 00:53:38 +0300

    Create V_219165.java

---
+++ src/tests/V_219165.java
@@ -0,0 +1,45 @@
+package tests;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExecuteShellComand {
+
+	public static void main(String[] args) {
+	    
+	    String V_219165 = "grep pam_lastlog /etc/pam.d/login";
+
+		ExecuteShellComand obj = new ExecuteShellComand();
+		
+		String output = obj.executeCommand(V_219165);
+
+		assert (output == "" || !output.contains("required") || output.contains("silent")) == false;
+	}
+
+	private String executeCommand(String command) {
+
+		StringBuffer output = new StringBuffer();
+
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader = 
+                           new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			String line = "";			
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return output.toString();
+
+	}
+
+}
